b019fc5c661b5dc2f93f91ef88b29c5a837c2a08
RakaKrisnaDwiPutra <rakaputra725203@gmail.com>
2023-04-06 04:43:32 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Raka</li>
+        <li>Nama Belakang : Putra</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Griya Bukit Jaya Blok E6/10</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Palembang</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>BasketBall</li>
+    </ul>
+</section>
